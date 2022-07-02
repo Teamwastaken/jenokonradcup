a3365397ef329c5e49abc3aa5612d8cd6502a84a
Erik <erik.p.schmitt@gmail.com>
2022-07-02 21:58:43 +0200

    About page.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import About from "./p/about";
 import Navigation from "./Navigation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Map from "./components/map";
@@ -11,6 +12,7 @@ root.render(
       <Navigation />
       <Routes>
         <Route path="/map/*" element={<Map />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ src/p/about.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import StartBild from "../img/9d-Gruppenbild.jpg";
+
+class About extends Component {
+  render() {
+    return (
+      <div className="w-5/6 m-auto p-4">
+        <img src={StartBild} alt="" />{" "}
+        <p>
+          Hallo, Wir sind die Klasse 9D des Sigmund-Schuckert-Gymnasiums in
+          Nürnberg.
+        </p>
+        <p>
+          {" "}
+          Diesen Sommer (2022) nahmen wir am Jenö Konrad – Cup des 1. FCN –
+          „Fussball trifft auf Geschichte“ teil. Vom Clubhistoriker Bernd
+          Siegler haben wir die Geschichte des jüdischen Clubtrainers Jenö
+          Konrad, der mit seiner Familie vor den Nationalsozialisten in die USA
+          flüchten musste, gehört.
+        </p>
+        <p>
+          Dies gab uns den Anstoß für ein eigenes Projekt zum Thema
+          Antisemitismus. Wir haben zusammen diese Stadtkarte mit Orten der
+          jüdischen Geschichte in Nürnberg erstellt, damit diese Orte nicht in
+          Vergessenheit geraten.
+        </p>
+        <p>
+          {" "}
+          Gleichzeitig haben wir gesehen, dass es auch heute noch Hass gegen
+          Juden gibt.
+        </p>
+        <p>
+          {" "}
+          Unsere Klasse hat mit Mitgliedern des Maccabi Frankfurt über Videochat
+          eine Konferenz abgehalten, in welcher diese uns über ihren Alltag als
+          Juden erzählt haben. Was uns echt erstaunt hat ist, dass
+          Antisemitismus immer noch so aktuell ist. Wir haben persönlich davon
+          eigentlich nichts mitbekommen, da Jude sein in unserem Umfeld nichts
+          daran ändert wie man behandelt wird, doch Noam Petri aus Frankfurt am
+          Main hat uns anderes erzählt. Ein paar echt verblüffende Sachen waren
+          unter anderem: Dass einer seiner Freunde den Kontakt mit ihm abbrach,
+          als er von seiner jüdischen Religion erfuhr, oder dass er seine
+          Davidsternkette in der Öffentlichkeit lieber nicht zeigt, da es schon
+          öfters zu gewalttätigen Übergriffen bei seinen Freunden kam. Wir haben
+          gegoogelt und die Fallzahlen haben uns überrascht: Die Zahl der
+          antisemitischen übergriffe 2021 lag bei mehr als 3.000; gegenüber 2020
+          sind die antisemitischen Straftaten um ganze 29% gestiegen.
+        </p>
+        <p> Doch was kann man dagegen tun? </p>
+        <p>
+          Um Antisemitismus an Schulen entgegenzuwirken, setzt sich der
+          "Unabhängige Expertenkreis" unter anderem für langfristige
+          Kooperationen von Schulen und jüdischen Verbänden ein. Im Juni 2021
+          legte die Kultusministerkonferenz gemeinsam mit dem Zentralrat der
+          Juden in Deutschland und der Bund-Länder-Kommission der
+          Antisemitismusbeauftragten Vorschläge vor: Schüler/innen und Lehrer
+          sollen Antisemitismus besser erkennen und Vorfälle als solche benennen
+          können. Es sollen Mechanismen geschaffen werden, dass Fälle gemeldet
+          und aufgearbeitet werden. Dafür sollen Beratungsnetzwerke eingebunden
+          werden. Die Empfehlungen sehen darüber hinaus Fortbildungen für
+          Lehrkräfte und eine Anpassung der Lehrpläne vor. Schüler sollen sich
+          etwa stärker mit der Vergangenheit und Gegenwart des Judentums
+          auseinandersetzen, um mehr Wissen über jüdisches Leben zu erlangen.
+        </p>
+        <p>
+          {" "}
+          Das haben wir mit diesem Projekt mit unserem Geschichtslehrer, Herrn
+          Sternecker, getan und viel gelernt. Einiges hat uns nachdenklich
+          gemacht…
+        </p>
+        <p>
+          {" "}
+          Wir wollen wachsam sein, dass sich die Geschichte nicht wiederholt und
+          niemand in unserem Land – aus welchen Gründen auch immer – ausgegrenzt
+          wird.
+        </p>
+        <p className="p-0">Liebe Grüße</p>
+        <p>
+          Eure Klasse 9 D Tom, Maxim, Benedikt, Ela, Michael, Martin, Leon,
+          Julia, Giuliano, Carla, Johanna, Jotta, Laura, Valerie, Diana,
+          Dalisha, Violetta, Simon, Sophie, Alina, Franziska, Anna, Simon, Erik,
+          Jil, Patrick und Herr Sternecker
+        </p>
+      </div>
+    );
+  }
+}
+
+export default About;
