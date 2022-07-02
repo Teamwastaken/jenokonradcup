9a714190c88c7a8ef7375c2e820346ec431cf9bf
Erik <erik.p.schmitt@gmail.com>
2022-07-02 22:14:32 +0200

    Added homepage.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import About from "./p/about";
-import Navigation from "./Navigation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Navigation from "./Navigation";
 import Map from "./components/map";
+import About from "./p/about";
+import Home from "./p/home";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Navigation />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/map/*" element={<Map />} />
         <Route path="/about" element={<About />} />
       </Routes>
---
+++ src/p/home.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import Synagoge from "../img/bild-startseite.jpeg";
+import JenoKonradCup from "../img/Jenoe-Konrad-Cup.png";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Link to="map">
+          <img className="w-5/6 m-auto p-10" src={Synagoge} alt="" />
+        </Link>
+        <p className="text-center w-5/6 m-auto pb-10">
+          Klicken Sie auf das Bild um zur Stadtkarte mit Orten der jüdischen
+          Geschichte zu gelangen.
+        </p>
+        <p className="text-center w-5/6 m-auto ">
+          Ein Projekt der Klasse 9D des Sigmund Schuckert Gynasium, Nürnberg
+          Eibach für den Jenö Konrad-Cup 2022
+        </p>
+        <img className="w-5/6 m-auto p-10" src={JenoKonradCup} alt="" />
+      </div>
+    );
+  }
+}
+
+export default Home;
