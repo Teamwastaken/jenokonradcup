9075c11061b5808221dfd94f917f804c946859b2
Erik <erik.p.schmitt@gmail.com>
2022-07-03 18:40:11 +0200

    Updated Hauptmarkt.

--- src/p/hauptmarkt.jsx
+++ src/p/hauptmarkt.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Marktplatz from "../img/marktplatz.jpeg";
 import Hero from "../img/Frauenkirche-verfremdet.png";
+import Judensau from "../img/Judensau.jpeg";
 class P1 extends Component {
   state = {};
   render() {
@@ -14,6 +15,7 @@ class P1 extends Component {
           große Verfolgung der jüdischen Minderheit mit Beginn der Kreuzzüge in
           Europa ein.
         </p>
+
         <p>
           Viele Juden flohen wegen Verfolgung im Rheinland 1146 nach Nürnberg.
           König Konrad III. gewährte ihnen Schutz und gab ihnen das
@@ -22,6 +24,17 @@ class P1 extends Component {
           Sie mussten an den ungesundesten Plätzen wohnen und wurden von
           Stechmücken geplagt. Die Synagoge stand an Stelle der heutigen
           Frauenkirche, umringt vom Getto.{" "}
+        </p>
+        <p>
+          Um 1240 hatte Nürnberg die größte süddeutsche jüdische Siedlung mit
+          über 1000 Personen. Die Rindfleisch-Verfolgung forderte 628 Männer,
+          Frauen und Kinder, die in Nürnberg ermordet wurden, auch einer der
+          wichtigsten jüdischen Gelehrten des Mittelalters: Mordchai Ben Hillel.
+          Diese Programme wurden möglich, da Adolf von Nassau und Albrecht von
+          Habsburg sich um den Königsthron stritten, wodurch es Unruhen und
+          Rechtsunsicherheit im Land gab. Der Gewinner des Thronstreits, König
+          Albrecht von Habsburg bestrafte 1928 die Aufrüher und sorgte dafür,
+          dass die jüdische Gemeinde erneut aufblühen konnte.
         </p>
         <p>
           Als man beide Städte zu einer zusammenfasste, standen die jüdische
@@ -33,6 +46,8 @@ class P1 extends Component {
           Frauenkirche, erbaut werden. Viele Juden hatten aus Sicherheitsgründen
           die Stadt bereits verlassen.{" "}
         </p>
+        <img src={Marktplatz} alt="" />
+        <p>Bild: Wikipedia</p>
         <p>
           Zunächst bemühte sich der Rat, Gewalttaten zu verhindern. Doch das
           Volk plünderte die jüdischen Häuser und bildete Spaliere und verhöhnte
@@ -43,8 +58,13 @@ class P1 extends Component {
           ließen sich wieder Juden in Nürnberg nieder, bis sie 1498 erneut
           vertrieben wurden.
         </p>
-        <img src={Marktplatz} alt="" />
-        <p>Bild: Wikipedia</p>
+        <img src={Judensau} alt="" />
+        <p>
+          Bild:{" "}
+          <a href="https://de.wikipedia.org/wiki/Datei:Judensau_nuernberg_sankt_sebald_kirche_2.jpg">
+            Wikipedia
+          </a>
+        </p>
         <p>
           Judenfeindliche Bauplastik einer "Judensau" am Ostchor der
           Sebalduskirche in Nürnberg. Dargestellt ist eine Sau, an deren Zitzen
@@ -71,6 +91,7 @@ class P1 extends Component {
           besser in einem Museum aufgehoben, als an der Wand einer Kirche und
           solle deshalb entfernt werden.
         </p>
+        <p>Ela, Laura, Dalisha</p>
       </div>
     );
   }
